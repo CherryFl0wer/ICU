0de3c526e172e20711ec88cd86c02516bf961feb
Drykso <adrien.943@gmail.com>
2015-10-05 21:44:34 +0200

    fonction test + debut fonction pple

---
+++ HaarFeatures.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#include"HaarFeatures.h"
+
+
+
+int main(int argc, char *argv[])
+{
+	int tab[5][5] = {{10,15,40,52,64},{15,42,1,45,65},{12,26,12,32,5},{24,45,65,95,2},{10,10,20,10,20}};
+	return 0;
+}
+
+
+void MakeVectWithFeat(HaarFeat* [],int tab[][])
+{
+	int length = sizeof(tab), width =sizeof(tab[0]);
+
+}	
---
+++ HaarFeatures.h
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+
+
+struct HaarFeat
+{
+	int feat,x,y,w,h,val;
+};
+
+
+void MakeVectWithFeat(HaarFeat [],int tab[][]);
