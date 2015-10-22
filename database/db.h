cf98807770f8b85f57a8c86dcba33fa596122add
Corentin <Gumcher@users.noreply.github.com>
2015-10-23 00:56:29 +0200

    Update db.h
    Add include in the header

--- database/db.h
+++ database/db.h
@@ -1,7 +1,9 @@
-
 #ifndef DATABASE_H_
 #define DATABASE_H_
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h> // Try to avoid stdbool and use int 0 = false 1 = true 
 
 void serialization(char name[20], FILE *database);
 void deserialization(char name[20]);
