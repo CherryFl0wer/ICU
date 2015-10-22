ad4bf821d4e61eda0f9c1dd0a7df65dba3f9f466
Corentin <Gumcher@users.noreply.github.com>
2015-10-23 00:54:38 +0200

    Update db.h
    Include stdio was not at the good place

--- database/db.h
+++ database/db.h
@@ -1,10 +1,12 @@
-# include <stdio.h>
+
 #ifndef DATABASE_H_
 #define DATABASE_H_
+#include <stdio.h>
 
 void serialization(char name[20], FILE *database);
 void deserialization(char name[20]);
 void modify(char name[20], char *n);
 void remov(char name[20]);
 static void print(FILE *f);
+
 #endif
