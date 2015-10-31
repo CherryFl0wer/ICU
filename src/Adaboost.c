71e3d901e6aa8a6b197d92fb9ae439814cbcb11a
Gumcher <redon_c@epita.fr>
2015-10-31 22:22:30 +0100

    Step 3 / 4 and Final StrongClassifier calculus + corrective

--- src/Adaboost.c
+++ src/Adaboost.c
@@ -1,12 +1,11 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-
 #include"AdaBoost.h"
-#include"HaarFeatures.h"
 
 int main()
 {
+		StrongClassifer* sc = malloc(sizeof(StrongClassifier));
+		Boost(sc, NULL, 200, 50);
+		free(sc);
+
     return 0;
 }
 
@@ -22,7 +21,6 @@ double sumWeight(double weights[],int size)
 void Boost(struct StrongClassifier *sc,struct ImgVal *img, int nbImg, int pos)
 {
     int t = 0;
-    int T = 200;
     int numImg = 0;
     struct HaarFeat* Haar = malloc(sizeof(struct HaarFeat));
     double weights[T][nbImg];
@@ -58,111 +56,110 @@ void Boost(struct StrongClassifier *sc,struct ImgVal *img, int nbImg, int pos)
             haar->h = 0;
             if(feat ==1)
             {
-                haar->feat = 1;
-        		for(int haar->x = 0; haar->x<24; haar->x++)
-	        	{
+              haar->feat = 1;
+        			for(int haar->x = 0; haar->x<24; haar->x++)
+	        		{
 		            for(int haar->y = 0; haar->y<24; haar->y++)
 		            {
 			            for(int haar->h = 1; haar->x+haar->h<24; haar->h++)
 			            {
-			                for(int haar->w = 1;haar->y+2*haar->w<24; haar->w++)
-			                {
-                    		    for(int i = 0;i<nbImg;i++)
-                                {
-                    		        ValFeatA(haar,img->[i].integ); 
-                                    img->wc[i].feature = haar;
-                                }
-                            }
+			              for(int haar->w = 1;haar->y+2*haar->w<24; haar->w++)
+			              {
+                    	for(int i = 0;i<nbImg;i++)
+                      {
+                    		ValFeatA(haar,img->wc[i].integ); 
+                        img->wc[i].feature = haar;
+                      }
+                    }
 			            }
 		            }
-	    	    }  
+	    	    	}  
             }
             else if(feat == 2)
             {
-                haar->feat = 2;
-        		for(int haar->x = 0; haar->x<24; haar->x++)
-	        	{
+           	  haar->feat = 2;
+        		  for(int haar->x = 0; haar->x<24; haar->x++)
+	        		{
 		            for(int haar->y = 0; haar->y<24; haar->y++)
 		            {
 			            for(int haar->h = 1; haar->x+haar->h<24; haar->h++)
 			            {
-			                for(int haar->w = 1;haar->y+3*haar->w<24; haar->w++)
-			                {
-                    		    for(int i = 0;i<nbImg;i++)
-                                {
-                    		        ValFeatB(haar,img->[i].integ); 
-                                    img->wc[i].feature = haar;
-                                }
-                            }
+			               for(int haar->w = 1;haar->y+3*haar->w<24; haar->w++)
+			               {
+                    		for(int i = 0;i<nbImg;i++)
+                        {
+                    			ValFeatB(haar,img->wc[i].integ); 
+                          img->wc[i].feature = haar;
+                        }
+                     }
 			            }
 		            }
-	    	    }
-
+	    	    	}
             }
             else if(feat == 3)
             {
-                haar->feat = 3;
-        		for(int haar->x = 0; haar->x<24; haar->x++)
-	        	{
+            	haar->feat = 3;
+        			for(int haar->x = 0; haar->x<24; haar->x++)
+	        		{
 		            for(int haar->y = 0; haar->y<24; haar->y++)
 		            {
 			            for(int haar->h = 1; haar->x+2*haar->h<24; haar->h++)
 			            {
 			                for(int haar->w = 1; haar->y+haar->w<24; haar->w++)
 			                {                    
-                    		    for(int i = 0;i<nbImg;i++)
-                                {
-                    		        ValFeatC(haar,img->[i].integ); 
-                                    img->wc[i].feature = haar;
-                                }
-                            }
-			            }
+                    		for(int i = 0;i<nbImg;i++)
+                        {
+                    			ValFeatC(haar,img->wc[i].integ); 
+                          img->wc[i].feature = haar;
+                        }
+                      }
+			            }	
 		            }
-	    	    }
+	    	    	}
 
             }
             else if(feat == 4)
             {
-                haar->feat = 4;
-        		for(int haar->x = 0; haar->x<24; haar->x++)
-	        	{
+            	haar->feat = 4;
+        			for(int haar->x = 0; haar->x<24; haar->x++)
+	        		{
 		            for(int haar->y = 0; haar->y<24; haar->y++)
 		            {
 			            for(int haar->h = 1; haar->x+3*haar->h<24; haar->h++)
 			            {
 			                for(int haar->w = 1; haar->y+haar->w<24; haar->w++)
 			                {
-                                for(int i = 0;i<nbImg;i++)
-                                {
-                    		        ValFeatD(haar,img->[i].integ); 
-                                    img->wc[i].feature = haar;
-                                }
+                      	for(int i = 0;i<nbImg;i++)
+                        {
+                    		  ValFeatD(haar,img->wc[i].integ); 
+                          img->wc[i].feature = haar;
+                        }
 			                }
 			            }
 		            }
-	    	    }
+	    	    	}
 
             }
             else if(feat == 5)
             {
-                haar->feat = 5;
-        		for(int haar->x = 0; haar->x<24; haar->x++)
-	        	{
+              haar->feat = 5;
+        			for(int haar->x = 0; haar->x<24; haar->x++)
+	        		{
 		            for(int haar->y = 0; haar->y<24; haar->y++)
 		            {
 			            for(int haar->h = 1; haar->x+2*haar->h<24; haar->h++)
 			            {
-			                for(int haar->w = 1;haar->y+2*haar->w<24; haar->w++)
-			                {
-                                for(int i = 0;i<nbImg;i++)
-                                {
-                    		        ValFeatE(haar,img->[i].integ); 
-                                    img->wc[i].feature = haar;
-                                }
-			                }
+			              for(int haar->w = 1;haar->y+2*haar->w<24; haar->w++)
+			              {
+                      for(int i = 0;i<nbImg;i++)
+                      {
+                    		ValFeatE(haar,img->wc[i].integ); 
+                        img->wc[i].feature = haar;
+                      }
+			              }
 			            }
 		            }
-	    	    }
+	    	   		}
 
             }
 
@@ -174,4 +171,54 @@ void Boost(struct StrongClassifier *sc,struct ImgVal *img, int nbImg, int pos)
 }
 
 
-void selectBestFeat(
+double alpha_calcul(int epsError) {
+	return log((1-epsError)/epsError);
+}
+
+
+// Don't know if it's better to replace by WeakClassifier wc
+void add_wc(StrongClassifier* sc, struct HaarFeat* feat, int threshold, int pol, int sw[24][24], int epsError, int round) {
+	WeakClassifier weak;
+	weak.feature 		= feat;
+	weak.threshold  = threshold;
+	weak.polarity 	= pol;
+	weak.integ 			= sw;
+	
+	sc->wc[round] 	= weak;
+	
+	// calcul alpha
+	sc->alpha[round] = alpha_calcul(epsError);
+}
+
+int wc_calcul(WeakClassifier* wc) {
+	return (wc->feature->val * wc->polarity < wc->threshold * wc->polarity) ? 1 : 0;  
+}
+
+void update_weight(int epsError, double weight[T][NBIMG], struct ImgVal* img, int posimg, int round) {
+	if(round < T) {
+		double beta = epsError / (1 - espError);
+		int ei = 0;
+		for(int i = 1; i < NBIMG; i++) { 
+			if(wc_calcul(img->wc[i]) * img->wc[i].polarity * -alpha_calcul(epsError) > 0)
+				ei = 1;
+			else
+				ei = 0;
+			
+			weight[round+1][i] = weight[round][i] * pow(beta, 1-ei);
+		} 
+		
+	}
+}
+
+
+// C(X) where X is an image. 
+// TODO : final_sc(SDL_Surface* img, StrongClassifier* sc) ??
+int	final_sc(StrongClassifier* sc) {
+	double sumSC = 0, sumAlpha = 0;
+	for(int i = 0; i < T; i++) {
+		sumSC += sc->alpha[i] * wc_calcul(sc->wc[round]);
+		sumAlpha += sc->alpha[i] * 0.5;
+	}
+
+	return (sumSC >= sumAlpha) ? 1 : 0;
+}
--- src/Adaboost.h
+++ src/Adaboost.h
@@ -3,28 +3,40 @@
 
 #include<stdlib.h>
 #include<stdio.h>
+#include<math.h> 
 #include"HaarFeatures.h"
 
+#define T 200 //nbr of round
+#define NBIMG 1000
 
 
-struct WeakClassifier { 
+typedef struct WeakClassifier { 
     struct HaarFeat* feature;
     int threshold;
     int integ[24][24];
     int polarity;
-};
+} WeakClassifier;
 
 struct ImgVal {
-    struct WeakClassifier wc[1000];// 1000 = number of image to test
+    WeakClassifier wc[NBIMG];// 1000 = number of image to test
+		int theta;
 }
 
 
-
-struct StrongClassifier {
-    struct WeakClassifier wc[200]; /* 200 = number of weakclassifier needed */
+typedef struct StrongClassifier {
+    WeakClassifier wc[200]; /* 200 = number of weakclassifier needed */
     int alpha[200]; /* same as above */
-};
+} StrongClassifier;
 
-void Boost(struct StrongClassifier *sc, int nbImg, int pos);
+void Boost(StrongClassifier *sc, int nbImg, int pos);
 
+void add_wc(StrongClassifer* sc, struct HaarFeat* feat, int seuil, int pol, int sw[24][24], int epsError, int round);
+
+double alpha_calcul(int epsError);
+
+int wc_calcul(WeakClassifier* wc);
+
+void update_weight(int epsError, double weight[T][NBIMG], struct ImgVal* img, int posimg, int round);
+
+int final_sc(StrongClassifier* sc);
 #endif
