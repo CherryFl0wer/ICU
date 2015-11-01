cfbdb8549401258fa979eec556c8bbb4ec8b5224
Gumcher <redon_c@epita.fr>
2015-11-01 01:36:38 +0100

    Fucking indentation

--- src/Adaboost.c
+++ src/Adaboost.c
@@ -4,338 +4,338 @@
 
 int main()
 {
-		StrongClassifer* sc = malloc(sizeof(StrongClassifier));
-		Boost(sc, NULL, 200, 50);
-		free(sc);
+  StrongClassifer* sc = malloc(sizeof(StrongClassifier));
+  Boost(sc, NULL, 200, 50);
+  free(sc);
 
-    return 0;
+  return 0;
 }
 
 double sumWeight(struct WeakClassifier *wc,int size)
 {
-    double res = 0.0;
-    for(int i = 0;i<size;i++)
-        res += wc[i].w;
-    return res;
+  double res = 0.0;
+  for(int i = 0;i<size;i++)
+    res += wc[i].w;
+  return res;
 
 }
 
 void Boost(struct StrongClassifier *sc,struct ImgVal *img, int nbImg, int pos)
 {
-    int t = 0;
-    int numImg = 0; 
-    double weights[T][nbImg];
-    double e = 2;
-    struct HaarFeat* haar = malloc(sizeof(struct HaarFeat));
+  int t = 0;
+  int numImg = 0; 
+  double weights[T][nbImg];
+  double e = 2;
+  struct HaarFeat* haar = malloc(sizeof(struct HaarFeat));
 
-    /* initialisation of weights */
-    for(int i = 0;i<pos;i++)
+  /* initialisation of weights */
+  for(int i = 0;i<pos;i++)
+  {
+    img->wc[i].w = 1/(2*pos);
+    (img->wc[i]).polarity = 1;
+  }
+
+  for(int i = pos;i<nbImg;i++)
+  {
+    img->wc[i].w = 1/(2*(nbImg-pos));
+    (img->wc[i]).polarity = -1;                
+  }
+
+
+
+  /* main loop */
+  for(;t<T;t++)
+  {
+    for(int i = 0;i<nbImg;i++)
     {
-        img->wc[i].w = 1/(2*pos);
-        (img->wc[i]).polarity = 1;
+      img->wc[i].w /= sumWeight(img->wc,nbImg);
     }
 
-    for(int i = pos;i<nbImg;i++)
+    /* select the best weak classifier */
+    for(int feat = 1;feat<=5;feat++)
     {
-        img->wc[i].w = 1/(2*(nbImg-pos));
-        (img->wc[i]).polarity = -1;                
-    }
+      haar->x = 0;
+      haar->y = 0;
+      haar->w = 0;
+      haar->h = 0;
 
-
-
-    /* main loop */
-    for(;t<T;t++)
-    {
-        for(int i = 0;i<nbImg;i++)
+      if(feat ==1)
+      {
+        haar->feat = 1;
+        for(haar->x = 0; haar->x<24; haar->x++)
         {
-            img->wc[i].w /= sumWeight(img->wc,nbImg);
+          for(haar->y = 0; haar->y<24; haar->y++)
+          {
+            for(haar->h = 1; haar->x+haar->h<24; haar->h++)
+            {
+              for(haar->w = 1;haar->y+2*haar->w<24; haar->w++)
+              {
+                for(int i = 0;i<nbImg;i++)
+                {
+                  ValFeatA(haar,img->wc[i].integ);
+                  img->wc[i].feature = haar;
+                  selectBestFeat(img,nbImg);
+                  if(img->e<e)
+                  {
+                    e = img->e;
+                    sc->wc[t] = img->wc[i];
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      else if(feat == 2)
+      {
+        haar->feat = 2;
+        for(haar->x = 0; haar->x<24; haar->x++)
+        {
+          for(haar->y = 0; haar->y<24; haar->y++)
+          {
+            for(haar->h = 1; haar->x+haar->h<24; haar->h++)
+            {
+              for(haar->w = 1;haar->y+3*haar->w<24; haar->w++)
+              {
+                for(int i = 0;i<nbImg;i++)
+                {
+                  ValFeatB(haar,img->wc[i].integ);
+                  img->wc[i].feature = haar;
+                  selectBestFeat(img,nbImg);
+                  if(img->e<e)
+                  {
+                    e = img->e;
+                    sc->wc[t] = img->wc[i];
+                  }
+                }
+              }
+            }
+          }
         }
 
-        /* select the best weak classifier */
-         for(int feat = 1;feat<=5;feat++)
-         {
-             haar->x = 0;
-             haar->y = 0;
-             haar->w = 0;
-             haar->h = 0;
-			     	
-					  	if(feat ==1)
-					  	{
-									haar->feat = 1;
-									for(haar->x = 0; haar->x<24; haar->x++)
-									{
-										for(haar->y = 0; haar->y<24; haar->y++)
-										{
-											for(haar->h = 1; haar->x+haar->h<24; haar->h++)
-											{
-												for(haar->w = 1;haar->y+2*haar->w<24; haar->w++)
-												{
-													for(int i = 0;i<nbImg;i++)
-													{
-														ValFeatA(haar,img->wc[i].integ);
-														img->wc[i].feature = haar;
-														selectBestFeat(img,nbImg);
-														if(img->e<e)
-														{
-															e = img->e;
-															sc->wc[t] = img->wc[i];
-														}
-													}
-												}
-											}
-										}
-									}
-						  	}
-								else if(feat == 2)
-								{
-									haar->feat = 2;
-									for(haar->x = 0; haar->x<24; haar->x++)
-									{
-										for(haar->y = 0; haar->y<24; haar->y++)
-										{
-											for(haar->h = 1; haar->x+haar->h<24; haar->h++)
-											{
-												for(haar->w = 1;haar->y+3*haar->w<24; haar->w++)
-												{
-													for(int i = 0;i<nbImg;i++)
-													{
-														ValFeatB(haar,img->wc[i].integ);
-														img->wc[i].feature = haar;
-														selectBestFeat(img,nbImg);
-														if(img->e<e)
-														{
-															e = img->e;
-															sc->wc[t] = img->wc[i];
-														}
-													}
-												}
-											}
-										}
-									}
-									
-								}
-								else if(feat == 3)
-								{
-									haar->feat = 3;
-									for(haar->x = 0; haar->x<24; haar->x++)
-									{
-										for(haar->y = 0; haar->y<24; haar->y++)
-										{
-											for(haar->h = 1; haar->x+2*haar->h<24; haar->h++)
-											{
-												for(haar->w = 1; haar->y+haar->w<24; haar->w++)
-												{
-													for(int i = 0;i<nbImg;i++)
-													{
-														ValFeatC(haar,img->wc[i].integ);
-														img->wc[i].feature = haar;
-														selectBestFeat(img,nbImg);
-														if(img->e<e)
-														{
-															e = img->e;
-															sc->wc[t] = img->wc[i];
-														}
-													}
-												}
-											}
-										}
-									}
-									
-								}
-								else if(feat == 4)
-								{
-									haar->feat = 4;
-									for(haar->x = 0; haar->x<24; haar->x++)
-									{
-										for(haar->y = 0; haar->y<24; haar->y++)
-										{
-											for(haar->h = 1; haar->x+3*haar->h<24; haar->h++)
-											{
-												for(haar->w = 1; haar->y+haar->w<24; haar->w++)
-												{
-													for(int i = 0;i<nbImg;i++)
-													{
-														ValFeatD(haar,img->wc[i].integ);
-														img->wc[i].feature = haar;
-														selectBestFeat(img,nbImg);
-														if(img->e<e)
-														{
-															e = img->e;
-															sc->wc[t] = img->wc[i];
-														}
-													}
-												}
-											}
-										}
-									}
-									
-								}
-								else if(feat == 5)
-								{
-									haar->feat = 5;
-									for(haar->x = 0; haar->x<24; haar->x++)
-									{
-										for(haar->y = 0; haar->y<24; haar->y++)
-										{
-											for(haar->h = 1; haar->x+2*haar->h<24; haar->h++)
-											{
-												for(haar->w = 1;haar->y+2*haar->w<24; haar->w++)
-												{
-													for(int i = 0;i<nbImg;i++)
-													{
-														ValFeatE(haar,img->wc[i].integ);
-														img->wc[i].feature = haar;
-														selectBestFeat(img,nbImg);
-														if(img->e<e)
-														{
-															e = img->e;
-															sc->wc[t] = img->wc[i];
-														}
-													}
-												}
-											}
-										}
-									}
-									
-								}
+      }
+      else if(feat == 3)
+      {
+        haar->feat = 3;
+        for(haar->x = 0; haar->x<24; haar->x++)
+        {
+          for(haar->y = 0; haar->y<24; haar->y++)
+          {
+            for(haar->h = 1; haar->x+2*haar->h<24; haar->h++)
+            {
+              for(haar->w = 1; haar->y+haar->w<24; haar->w++)
+              {
+                for(int i = 0;i<nbImg;i++)
+                {
+                  ValFeatC(haar,img->wc[i].integ);
+                  img->wc[i].feature = haar;
+                  selectBestFeat(img,nbImg);
+                  if(img->e<e)
+                  {
+                    e = img->e;
+                    sc->wc[t] = img->wc[i];
+                  }
+                }
+              }
+            }
+          }
+        }
 
-						}
+      }
+      else if(feat == 4)
+      {
+        haar->feat = 4;
+        for(haar->x = 0; haar->x<24; haar->x++)
+        {
+          for(haar->y = 0; haar->y<24; haar->y++)
+          {
+            for(haar->h = 1; haar->x+3*haar->h<24; haar->h++)
+            {
+              for(haar->w = 1; haar->y+haar->w<24; haar->w++)
+              {
+                for(int i = 0;i<nbImg;i++)
+                {
+                  ValFeatD(haar,img->wc[i].integ);
+                  img->wc[i].feature = haar;
+                  selectBestFeat(img,nbImg);
+                  if(img->e<e)
+                  {
+                    e = img->e;
+                    sc->wc[t] = img->wc[i];
+                  }
+                }
+              }
+            }
+          }
+        }
+
+      }
+      else if(feat == 5)
+      {
+        haar->feat = 5;
+        for(haar->x = 0; haar->x<24; haar->x++)
+        {
+          for(haar->y = 0; haar->y<24; haar->y++)
+          {
+            for(haar->h = 1; haar->x+2*haar->h<24; haar->h++)
+            {
+              for(haar->w = 1;haar->y+2*haar->w<24; haar->w++)
+              {
+                for(int i = 0;i<nbImg;i++)
+                {
+                  ValFeatE(haar,img->wc[i].integ);
+                  img->wc[i].feature = haar;
+                  selectBestFeat(img,nbImg);
+                  if(img->e<e)
+                  {
+                    e = img->e;
+                    sc->wc[t] = img->wc[i];
+                  }
+                }
+              }
+            }
+          }
+        }
+
+      }
+
+    }
 
 
-     }  
-     free(haar);
+  }  
+  free(haar);
 }
 
 
 double alpha_calcul(int epsError) {
-	return log((1-epsError)/epsError);
+  return log((1-epsError)/epsError);
 }
 
 
 // Don't know if it's better to replace by WeakClassifier wc
 void add_wc(StrongClassifier* sc, struct HaarFeat* feat, int threshold, int pol, int sw[24][24], int epsError, int round) {
-	WeakClassifier weak;
-	weak.feature 		= feat;
-	weak.threshold  = threshold;
-	weak.polarity 	= pol;
-	weak.integ 			= sw;
-	
-	sc->wc[round] 	= weak;
-	
-	// calcul alpha
-	sc->alpha[round] = alpha_calcul(epsError);
+  WeakClassifier weak;
+  weak.feature 		= feat;
+  weak.threshold  = threshold;
+  weak.polarity 	= pol;
+  weak.integ 			= sw;
+
+  sc->wc[round] 	= weak;
+
+  // calcul alpha
+  sc->alpha[round] = alpha_calcul(epsError);
 }
 
 int wc_calcul(WeakClassifier* wc) {
-	return (wc->feature->val * wc->polarity < wc->threshold * wc->polarity) ? 1 : 0;  
+  return (wc->feature->val * wc->polarity < wc->threshold * wc->polarity) ? 1 : 0;  
 }
 
 void update_weight(int epsError, double weight[T][NBIMG], struct ImgVal* img, int posimg, int round) {
-	if(round < T) {
-		double beta = epsError / (1 - espError);
-		int ei = 0;
-		for(int i = 1; i < NBIMG; i++) { 
-			if(wc_calcul(img->wc[i]) * img->wc[i].polarity * -alpha_calcul(epsError) > 0)
-				ei = 1;
-			else
-				ei = 0;
-			
-			weight[round+1][i] = weight[round][i] * pow(beta, 1-ei);
-		} 
-		
-	}
+  if(round < T) {
+    double beta = epsError / (1 - espError);
+    int ei = 0;
+    for(int i = 1; i < NBIMG; i++) { 
+      if(wc_calcul(img->wc[i]) * img->wc[i].polarity * -alpha_calcul(epsError) > 0)
+        ei = 1;
+      else
+        ei = 0;
+
+      weight[round+1][i] = weight[round][i] * pow(beta, 1-ei);
+    } 
+
+  }
 }
 
 
 // C(X) where X is an image. 
 // TODO : final_sc(SDL_Surface* img, StrongClassifier* sc) ??
 int	final_sc(StrongClassifier* sc) {
-	double sumSC = 0, sumAlpha = 0;
-	for(int i = 0; i < T; i++) {
-		sumSC += sc->alpha[i] * wc_calcul(sc->wc[round]);
-		sumAlpha += sc->alpha[i] * 0.5;
-	}
+  double sumSC = 0, sumAlpha = 0;
+  for(int i = 0; i < T; i++) {
+    sumSC += sc->alpha[i] * wc_calcul(sc->wc[round]);
+    sumAlpha += sc->alpha[i] * 0.5;
+  }
 
-	return (sumSC >= sumAlpha) ? 1 : 0;
+  return (sumSC >= sumAlpha) ? 1 : 0;
 }
 
 
 void selectBestFeat(struct ImgVal *img,int nbImg)
 {
-//    sorted img + weights
-    sort(img);
-    double Sp = 0,Sn = 0, Tp = 0, Tn = 0;
-    int T = 0;
-    int t = img->wc[0].feature->val;
-    int M = 0;
-    double e = 2;
+  //    sorted img + weights
+  sort(img);
+  double Sp = 0,Sn = 0, Tp = 0, Tn = 0;
+  int T = 0;
+  int t = img->wc[0].feature->val;
+  int M = 0;
+  double e = 2;
 
-    for(int i = 0;i<nbImg;i++)
+  for(int i = 0;i<nbImg;i++)
+  {
+    if(img->wc[i].polarity == 1)
+      Tp += img->wc[i].w;
+    else
+      Tn += img->wc[i].w;
+  }
+  int jIter = 0;
+  int tIter = t;
+  int MIter = M;
+  int TIter;
+  double eIter;
+  double Ep,En;
+  while(1){
+    Ep = Sp+Tn;
+    En = Tp+Sn;
+    if(Ep<En)
     {
-        if(img->wc[i].polarity == 1)
-            Tp += img->wc[i].w;
-        else
-            Tn += img->wc[i].w;
+      eIter = Ep;
+      TIter = 1;
     }
-    int jIter = 0;
-    int tIter = t;
-    int MIter = M;
-    int TIter;
-    double eIter;
-    double Ep,En;
+    else
+    {
+      eIter = En;
+      TIter = -1;
+    }
+    if(eIter<=e && MIter>M)
+    {
+      e = eIter;
+      t = tIter;
+      M = MIter;
+      T = TIter;
+    }
+    if(jIter == nbImg)
+      break;
+    jIter++;
     while(1){
-        Ep = Sp+Tn;
-        En = Tp+Sn;
-        if(Ep<En)
-        {
-            eIter = Ep;
-            TIter = 1;
-        }
-        else
-        {
-            eIter = En;
-            TIter = -1;
-        }
-        if(eIter<=e && MIter>M)
-        {
-            e = eIter;
-            t = tIter;
-            M = MIter;
-            T = TIter;
-        }
-        if(jIter == nbImg)
-            break;
+      if(img->wc[jIter].polarity == -1)
+      {
+        Sn += img->wc[jIter].w;
+        Tn -= img->wc[jIter].w;
+      }
+      else
+      {
+        Sp += img->wc[jIter].w;
+        Tp -= img->wc[jIter].w;
+      }
+      if(jIter == nbImg || img->wc[jIter].feature->val)
+        break;
+      else
         jIter++;
-        while(1){
-            if(img->wc[jIter].polarity == -1)
-            {
-                Sn += img->wc[jIter].w;
-                Tn -= img->wc[jIter].w;
-            }
-            else
-            {
-                Sp += img->wc[jIter].w;
-                Tp -= img->wc[jIter].w;
-            }
-            if(jIter == nbImg || img->wc[jIter].feature->val)
-                break;
-            else
-                jIter++;
-        }
-        if(jIter == nbImg)
-        {
-            tIter = img->wc[jIter].feature->val + 1;
-            MIter = 0;
-        }
-        else
-        {
-            tIter=(img->wc[jIter].feature->val+img->wc[jIter+1].feature->val)/2;
-            MIter = img->wc[jIter+1].feature->val -img->wc[jIter].feature->val;
-        }
     }
-    img->theta = t;
-    img->e=e;
-    img->M=M;
-    img->T=T;
+    if(jIter == nbImg)
+    {
+      tIter = img->wc[jIter].feature->val + 1;
+      MIter = 0;
+    }
+    else
+    {
+      tIter=(img->wc[jIter].feature->val+img->wc[jIter+1].feature->val)/2;
+      MIter = img->wc[jIter+1].feature->val -img->wc[jIter].feature->val;
+    }
+  }
+  img->theta = t;
+  img->e=e;
+  img->M=M;
+  img->T=T;
 
 }
