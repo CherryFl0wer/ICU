2e80cd832dde955a1b5010a3b64ac9be6648f495
Baptiste Fortin <baptiste.fortin@epita.fr>
2015-12-02 02:30:23 +0100

    test-gtk

--- gtk/gtk.c
+++ gtk/gtk.c
@@ -2,33 +2,37 @@
 #include <gtk/gtk.h> 
 #include <stdio.h>
 
-
 void OnDestroy(GtkWidget *pWidget, gpointer pData);
+void AddBtn(GtkWidget *pWindow);
+//void gtk_button_get_always_show_image(GtkButton *button);
 
 int main(int argc,char **argv)
 { 
-/* Déclaration du widget */
+  /* Déclaration du widget */
   GtkWidget *pWindow;
   gtk_init(&argc,&argv);
 
-/* Création de la fenêtre */
+  /* Création de la fenêtre */
   pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_position(GTK_WINDOW(pWindow), GTK_WIN_POS_MOUSE);
   gtk_window_set_default_size(GTK_WINDOW(pWindow), 320, 200);
-  gtk_window_set_title(GTK_WINDOW(pWindow), "ICU");
-  //gtk_window_set_decorated(GTK_WINDOW(pWindow), TRUE);
+  // gtk_window_set_title(GTK_WINDOW(pWindow), "ICU");
 
-/* Connexion du signal "destroy" */
-  g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(OnDestroy), NULL);
+  /* Connexion du signal "destroy" */
+  g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-/* Affichage de la fenêtre */
+  /* Insertion du bouton */
+  AddBtn(pWindow);
+
+  /* Affichage de la fenêtre */
   gtk_widget_show(pWindow);
 
-/* Demarrage de la boucle evenementielle */
+  /* Demarrage de la boucle evenementielle */
   gtk_main();
 
-return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
+
 
 void OnDestroy(GtkWidget *pWidget, gpointer pData)
 {
@@ -37,3 +41,23 @@ void OnDestroy(GtkWidget *pWidget, gpointer pData)
 /* Arret de la boucle evenementielle */
   gtk_main_quit();
 }
+
+void AddBtn(GtkWidget *pWindow)
+{
+  GtkWidget *pQuitBtn;
+  GtkWidget *pImage;
+   
+  //init image
+  pImage = gtk_image_new_from_file("/home/eriovi/Téléchargements/door-in-icon.png");
+  
+  //init button
+  pQuitBtn = gtk_button_new();
+  gtk_button_set_label(GTK_BUTTON(pQuitBtn), "Database");
+  gtk_button_set_image(GTK_BUTTON(pQuitBtn), pImage);
+//  gtk_button_get_always_show_image(GTK_BUTTON(pQuitBtn));
+   
+  /* Insertion du bouton dans la fenetre */
+  gtk_container_add(GTK_CONTAINER(pWindow), pQuitBtn);
+}
+
+
---
+++ gtk/gtk_test.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <gtk/gtk.h> 
+#define EXEMPLE_1 0
+#define EXEMPLE_2 1
+#define EXEMPLE_3 2
+
+void AddBtn(GtkWidget *pWindow, gint iExemple);
+void gtk_button_set_always_show_image(GtkButton *button, gboolean always_show);
+
+int main(int argc, char **argv)
+{ 
+  GtkWidget* pWindow;
+  gtk_init(&argc, &argv);
+  /* Creation de la fenetre */
+  pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(pWindow), 320 ,200); 
+
+  /* Connexion du signal "destroy" de la fenetre */
+  g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  /* Insertion du bouton */
+  AddBtn(pWindow, EXEMPLE_1);
+  /* Affichage de la fenetre */
+  gtk_widget_show_all(pWindow); 
+  /* Demarrage de la boucle evenementielle */
+  gtk_main();
+  return EXIT_SUCCESS; 
+}
+/*
+ *   void AddBtn(GtkWidget *pWindow, gint iExemple)
+ *     
+ *       Fonction en charge d'inserer le bouton dans la fenetre
+ *        
+ *          Parametre :
+ *            - pWindow : fenetre parente
+ *              - iExemple : mode de creation 
+ *                EXEMPLE_1 pour un bouton label
+ *                  EXEMPLE_2 pour un bouton EXEMPLE_1 + raccourci
+ *                    EXEMPLE_3 pour un bouton EXEMPLE_2 + image
+ *                    */
+void AddBtn(GtkWidget *pWindow, gint iExemple)
+{
+  iExemple = 0;
+
+  GtkWidget *pQuitBtn;
+//  GtkWidget *pLabel;
+  GtkWidget *pImage;
+
+  //init image
+  pImage = gtk_image_new_from_file("/home/eriovi/Téléchargements/door-in-icon.png");
+
+  //init label
+//  pLabel = gtk_label_new("QUIT");
+  
+  //init button
+  pQuitBtn = gtk_button_new();
+//  gtk_container_add(GTK_CONTAINER(pQuitBtn), pLabel);
+  gtk_button_set_label(GTK_BUTTON(pQuitBtn), "QUIT");
+  gtk_button_set_image(GTK_BUTTON(pQuitBtn), pImage);
+
+//  GtkSettings *default_settings = gtk_setting_get_default();
+//  g_object_set(default_settings, "gtk-button-images", TRUE, NULL);
+
+  gtk_button_set_always_show_image(GTK_BUTTON(pQuitBtn), TRUE);
+
+  gtk_window_set_title(GTK_WINDOW(pWindow), "Les boutons - Exemple 1");
+
+
+  /* Connexion du signal "clicked" du bouton */
+  g_signal_connect(G_OBJECT(pQuitBtn), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+  /* Insertion du bouton dans la fenetre */
+  gtk_container_add(GTK_CONTAINER(pWindow), pQuitBtn);
+}
+
