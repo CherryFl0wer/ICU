ebddd4a2136a971ee1aba429e614b63f5157ebd1
Baptiste Fortin <baptiste.fortin@epita.fr>
2015-12-04 17:27:33 +0100

    gtk

--- gtk/gtk.c
+++ gtk/gtk.c
@@ -1,29 +1,34 @@
 #include <stdlib.h>
 #include <gtk/gtk.h> 
 
-void AddBtn(GtkWidget *pWindow);
+void identification()
+{
+  GtkWidget* iWindow;
 
-int main(int argc, char **argv)
+  // creation of the window
+  iWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(iWindow), 400, 450); 
+  gtk_window_set_position(GTK_WINDOW(iWindow), GTK_WIN_POS_MOUSE);
+  gtk_window_set_title(GTK_WINDOW(iWindow), "Identification");
+  gtk_container_set_border_width(GTK_CONTAINER(iWindow), 15);
+
+  printf("clicked identification button\n");
+  gtk_widget_show_all(iWindow);
+}
+
+void database()
 { 
-  GtkWidget* pWindow;
-  gtk_init(&argc, &argv);
+  GtkWidget* dWindow;
 
-  /* Creation de la fenetre */
-  pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size(GTK_WINDOW(pWindow), 400, 450); 
-  gtk_window_set_position(GTK_WINDOW(pWindow), GTK_WIN_POS_CENTER);
-  gtk_window_set_title(GTK_WINDOW(pWindow), "ICU");
-  gtk_container_set_border_width(GTK_CONTAINER(pWindow), 15);
+  // creation of the window
+  dWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(dWindow), 400, 450); 
+  gtk_window_set_position(GTK_WINDOW(dWindow), GTK_WIN_POS_MOUSE);
+  gtk_window_set_title(GTK_WINDOW(dWindow), "Database");
+  gtk_container_set_border_width(GTK_CONTAINER(dWindow), 15);
 
-  /* Connexion du signal "destroy" de la fenetre */
-  g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-  /* Insertion du bouton */
-  AddBtn(pWindow);
-  /* Affichage de la fenetre */
-  gtk_widget_show_all(pWindow); 
-  /* Demarrage de la boucle evenementielle */
-  gtk_main();
-  return EXIT_SUCCESS; 
+  printf("clicked database button\n");
+  gtk_widget_show_all(dWindow); 
 }
 
 void AddBtn(GtkWidget *pWindow)
@@ -52,7 +57,7 @@ void AddBtn(GtkWidget *pWindow)
   gtk_button_set_label(GTK_BUTTON(pQuitBtn), "Quit");
   gtk_button_set_image(GTK_BUTTON(pQuitBtn), pQuitIm);
   gtk_button_set_image_position(GTK_BUTTON(pQuitBtn), GTK_POS_TOP);
-
+  
 
   // init button data
   pDataBtn = gtk_button_new();
@@ -71,10 +76,36 @@ void AddBtn(GtkWidget *pWindow)
   gtk_button_set_always_show_image(GTK_BUTTON(pDataBtn), TRUE);
   gtk_button_set_always_show_image(GTK_BUTTON(pIdenBtn), TRUE);
 
+  // button in the box
   gtk_box_pack_start(GTK_BOX(box), pIdenBtn, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(box), pDataBtn, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(box), pQuitBtn, TRUE, TRUE, 0);
 
-  // Connexion du signal "clicked" du bouton
+  // button signal
   g_signal_connect(G_OBJECT(pQuitBtn), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(G_OBJECT(pDataBtn), "clicked", G_CALLBACK(database), NULL);
+  g_signal_connect(G_OBJECT(pIdenBtn), "clicked", G_CALLBACK(identification), NULL);
+}
+
+int main(int argc, char **argv)
+{ 
+  GtkWidget* pWindow;
+  gtk_init(&argc, &argv);
+
+  /* Creation de la fenetre */
+  pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(pWindow), 400, 450); 
+  gtk_window_set_position(GTK_WINDOW(pWindow), GTK_WIN_POS_CENTER);
+  gtk_window_set_title(GTK_WINDOW(pWindow), "ICU");
+  gtk_container_set_border_width(GTK_CONTAINER(pWindow), 15);
+
+  /* Connexion du signal "destroy" de la fenetre */
+  g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  /* Insertion du bouton */
+  AddBtn(pWindow);
+  /* Affichage de la fenetre */
+  gtk_widget_show_all(pWindow); 
+  /* Demarrage de la boucle evenementielle */
+  gtk_main();
+  return EXIT_SUCCESS; 
 }
