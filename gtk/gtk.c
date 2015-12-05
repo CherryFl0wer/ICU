cb833e5b0467449bbc6d038e43d7556d06dfbc2f
Baptiste Fortin <baptiste.fortin@epita.fr>
2015-12-05 21:41:25 +0100

    database++ and clean

--- gtk/gtk.c
+++ gtk/gtk.c
@@ -33,24 +33,104 @@ enum
   N_COLUMNS
 };
 
+GtkWidget *list;
+
+/*** entry ***/
+void append_item(GtkWidget *widget, gpointer entry) 
+{
+  GtkListStore *store;
+  GtkTreeIter iter;
+
+  const gchar *str = gtk_entry_get_text(entry); 
+
+  store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(list)));
+
+  gtk_list_store_append(store, &iter);
+  gtk_list_store_set(store, &iter, LIST_ITEM, str, -1);
+
+  gtk_entry_set_text(entry, "");
+  widget = 0;
+}
+
+/*** remove ***/
+void remove_item(GtkWidget *widget, gpointer selection) 
+{
+  GtkListStore *store;
+  GtkTreeModel *model;
+  GtkTreeIter  iter;
+
+  store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(list)));
+  model = gtk_tree_view_get_model(GTK_TREE_VIEW(list));
+
+  if (gtk_tree_model_get_iter_first(model, &iter) == FALSE) {
+    return;
+  }
+
+  if (gtk_tree_selection_get_selected(GTK_TREE_SELECTION(selection), 
+        &model, &iter)) {
+    gtk_list_store_remove(store, &iter);
+  }
+  widget = 0;
+}
+
+/*** remove all ***/
+void remove_all(GtkWidget *widget/*, gpointer selection*/) 
+{
+  GtkListStore *store;
+  GtkTreeModel *model;
+  GtkTreeIter  iter;
+
+  store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(list)));
+  model = gtk_tree_view_get_model(GTK_TREE_VIEW(list));
+
+  if (gtk_tree_model_get_iter_first(model, &iter) == FALSE) {
+    return;
+  }
+
+  gtk_list_store_clear(store);
+  widget = 0;
+}
+
+/*** Init ***/
 void init_list(GtkWidget *list) 
 {
-  GtkCellRenderer *renderer;
-  GtkTreeViewColumn *column;
-  GtkListStore *store;
+  GtkCellRenderer    *renderer;
+  GtkTreeViewColumn  *column;
+  GtkListStore       *store;
 
-  renderer = gtk_cell_renderer_text_new ();
-  column = gtk_tree_view_column_new_with_attributes("List Items",
+  renderer = gtk_cell_renderer_text_new();
+  column = gtk_tree_view_column_new_with_attributes("List Item",
       renderer, "text", LIST_ITEM, NULL);
   gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
 
   store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING);
 
-  gtk_tree_view_set_model(GTK_TREE_VIEW(list), 
-      GTK_TREE_MODEL(store));
+  gtk_tree_view_set_model(GTK_TREE_VIEW(list), GTK_TREE_MODEL(store));
 
   g_object_unref(store);
 }
+
+/*
+   void init_list(GtkWidget *list) 
+   {
+   GtkCellRenderer *renderer;
+   GtkTreeViewColumn *column;
+   GtkListStore *store;
+
+   renderer = gtk_cell_renderer_text_new ();
+   column = gtk_tree_view_column_new_with_attributes("List Items",
+   renderer, "text", LIST_ITEM, NULL);
+   gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+
+   store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING);
+
+   gtk_tree_view_set_model(GTK_TREE_VIEW(list), 
+   GTK_TREE_MODEL(store));
+
+   g_object_unref(store);
+   }
+   */
+
 
 void add_database(GtkWidget *list)
 {
@@ -78,6 +158,8 @@ void add_database(GtkWidget *list)
   fclose(db);
 }
 
+
+/*
 void on_changed(GtkWidget *widget, gpointer label) 
 {
 
@@ -93,36 +175,70 @@ void on_changed(GtkWidget *widget, gpointer label)
     g_free(value);
   }
 }
+*/
 
 void database()
 { 
   GtkWidget* dWindow;
-  GtkWidget *list;
+  GtkWidget *sw;
+  // GtkWidget *list;
+
+  GtkWidget *remove;
+  GtkWidget *add;
+  GtkWidget *removeAll;
+  GtkWidget *entry;
 
   GtkWidget *box;
-  GtkWidget *label;
+  GtkWidget *hbox;
+  // GtkWidget *label;
   GtkTreeSelection *selection;
 
   // creation of the window
   dWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size(GTK_WINDOW(dWindow), 270, 250); 
+  gtk_window_set_default_size(GTK_WINDOW(dWindow), 370, 270); 
   gtk_window_set_position(GTK_WINDOW(dWindow), GTK_WIN_POS_MOUSE);
   gtk_window_set_title(GTK_WINDOW(dWindow), "Database");
   gtk_container_set_border_width(GTK_CONTAINER(dWindow), 10);
 
+  // init sw
+  sw = gtk_scrolled_window_new(NULL, NULL);
   list = gtk_tree_view_new();
+  gtk_container_add(GTK_CONTAINER(sw), list);
+
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw),
+      GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+
+  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw),
+      GTK_SHADOW_ETCHED_IN);
 
   gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), FALSE);
 
   // init box
   box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-  gtk_box_pack_start(GTK_BOX(box), list, TRUE, TRUE, 5);
+  gtk_box_pack_start(GTK_BOX(box), sw, TRUE, TRUE, 5);
 
-  // init list
-  label = gtk_label_new("");
-  gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 5);
+  // init hbox
+  hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+
+  // init buttons
+  add = gtk_button_new_with_label("Add");
+  remove = gtk_button_new_with_label("Remove");
+  removeAll = gtk_button_new_with_label("Remove All");
+  entry = gtk_entry_new();
+  gtk_widget_set_size_request(entry, 120, -1);
+
+  gtk_box_pack_start(GTK_BOX(hbox), add, FALSE, TRUE, 3);
+  gtk_box_pack_start(GTK_BOX(hbox), entry, FALSE, TRUE, 3);
+  gtk_box_pack_start(GTK_BOX(hbox), remove, FALSE, TRUE, 3);
+  gtk_box_pack_start(GTK_BOX(hbox), removeAll, FALSE, TRUE, 3);
+
+  gtk_box_pack_start(GTK_BOX(box), hbox, FALSE, TRUE, 3);
 
   gtk_container_add(GTK_CONTAINER(dWindow), box);
+
+  // init list
+  // label = gtk_label_new("");
+  // gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 5);
 
   // init list (suite)
   init_list(list);
@@ -131,9 +247,22 @@ void database()
   // init tree
   selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(list));
 
-  g_signal_connect(selection, "changed", G_CALLBACK(on_changed), label);
+  // test buttons clicked
+  g_signal_connect(/*G_OBJECT(add)*/GTK_BUTTON(add), "clicked",
+      G_CALLBACK(append_item), entry);
 
-  printf("clicked database button\n");
+  g_signal_connect(G_OBJECT(remove), "clicked",
+      G_CALLBACK(remove_item), selection);
+
+  g_signal_connect(G_OBJECT(removeAll), "clicked",
+      G_CALLBACK(remove_all), selection);
+
+  //g_signal_connect(G_OBJECT(dWindow), "destroy",
+  //    G_CALLBACK(gtk_main_quit), NULL);
+
+  // g_signal_connect(selection, "changed", G_CALLBACK(on_changed), label);
+
+  // printf("clicked database button\n");
   gtk_widget_show_all(dWindow); 
 }
 
